7a74c959b16259fb68f26ca10f742a48141470d3
WTFBlaze <35648047+WTFBlaze@users.noreply.github.com>
2023-03-04 11:54:49 -0500

    started project

---
+++ BossMod/Main.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(BossMod.Main), "BossMod", "1.0.0", "WTFBlaze")]
+[assembly: MelonGame("Playside Studios", "WorldBoss")]
+
+namespace BossMod
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            
+        }
+    }
+}
